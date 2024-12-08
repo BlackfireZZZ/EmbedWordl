92a055bf7fa7a080106f5c67fe9ea6e37f2d7786
Blackfire <Stipundrik@gmail.com>
2024-12-09 02:55:54 +0300

    Docker fixes

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -4,7 +4,7 @@ import gensim
 
 app = Flask(__name__)
 mystem = Mystem()
-model_path = 'C:/Users/79150/Documents/programming/EmbedWordl/backend/application/embedmodel/cc.ru.300.bin.gz'
+model_path = '/app/application/embedmodel/cc.ru.300.bin.gz'
 model = gensim.models.fasttext.load_facebook_vectors(model_path)
 
 from application import routes
